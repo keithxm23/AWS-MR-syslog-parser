de7776874f33a3f6cc0d58ebb36f227c0f07beea
Keith Mascarenhas <keithxm23@gmail.com>
2013-10-09 23:23:12 -0400

    several major improvements. now using prettytables. added more fields.

--- parse.py
+++ parse.py
@@ -4,8 +4,10 @@ __author__ = "Keith Mascarenhas"
 __email__ = "keithxm23@gmail.com"
 
 import os
-from datetime import timedelta
 import datetime
+from datetime import timedelta
+from prettytable import PrettyTable # easy_install prettytable OR Look up https://code.google.com/p/prettytable/wiki/Installation
+from operator import attrgetter
 data = {}
 for r,d,f in os.walk("./"): #Either point to directory containing syslog files or place this file in that directory
     for file in f:
@@ -31,8 +33,18 @@ for r,d,f in os.walk("./"): #Either point to directory containing syslog files o
 					
 				data[file]['Job Runtime'] =  str("%.2f" % ((end - start).seconds/60.0))
 
+
 ### Generating a simple report ###
-# Syslog Filename | Cpu time spent (ms) | Cpu time spent (minutes) | Combine input records | Combine input records
-print 'Syslog Name \t\t| JobRunningTime(mins) \t| CPU(ms) \t| CPU(mins) \t| Combine IP | Combine OP'
+pt = PrettyTable(["Program", "Job Running Time (mins)", "CPU Time Spent (ms)", "CPU Time Spent (mins)", "CIR", "COR", "MIR", "MOR", "RIR", "ROR"])
+pt.align["Program"] = "l" # Left align program names
+pt.padding_width = 1 # One space between column edges and contents (default)
 for f in data:
-	print f+'\t|\t'+data[f]['Job Runtime']+'\t\t|'+data[f]['CPU time spent (ms)']+'\t|\t'+str("%.2f" % (float(data[f]['CPU time spent (ms)'])/60000.0))+' \t|  '+data[f]['Combine input records']+' | '+data[f]['Combine output records']
+	pt.add_row([f, data[f]['Job Runtime'], data[f]['CPU time spent (ms)'], str("%.2f" % (float(data[f]['CPU time spent (ms)'])/60000.0)), data[f]['Combine input records'], data[f]['Combine output records'], data[f]['Map input records'], data[f]['Map output records'], data[f]['Reduce input records'], data[f]['Reduce output records']])
+print pt.get_string(sortby="Program")
+print """Key:
+CIR = Combine Input Records
+COR = Combine Output Records
+MIR = Map Input Records
+MOR = Map Output Records
+RIR = Reduce Input Records
+ROR = Reduce Output Records"""
